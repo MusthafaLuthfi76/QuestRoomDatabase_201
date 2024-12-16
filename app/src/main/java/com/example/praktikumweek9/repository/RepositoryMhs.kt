10c08961b480ff18b2310712abcdca71ed3c118c
MusthafaLuthfi76 <musthafa.luthfi76@gmail.com>
2024-12-16 21:57:45 +0700

    Menambahkan kode di RepositoryMhs untuk mengelola data mahasiswa untuk memanggil fungsi fungsi crud yang dibuat di mahasiswadao

--- app/src/main/java/com/example/praktikumweek9/repository/RepositoryMhs.kt
+++ app/src/main/java/com/example/praktikumweek9/repository/RepositoryMhs.kt
@@ -1,7 +1,16 @@
 package com.example.praktikumweek9.repository
 
 import com.example.praktikumweek9.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 interface RepositoryMhs {
     suspend fun insertMhs(mahasiswa: Mahasiswa)
+
+    fun getAllMhs (): Flow<List<Mahasiswa>>
+
+    fun getMhs(nim: String): Flow<Mahasiswa>
+
+    suspend fun deleteMhs (mahasiswa: Mahasiswa)
+
+    suspend fun updateMhs(mahasiswa: Mahasiswa)
 }
